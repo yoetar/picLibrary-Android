163bc2df515204d4cf65cd13622921ba437c71a3
yoetar <62800794+yoetar@users.noreply.github.com>
2020-03-28 17:06:37 +0100

    Update Project 03/28/2020

--- app/src/main/java/yoetar/picLibrary/MainActivity.java
+++ app/src/main/java/yoetar/picLibrary/MainActivity.java
@@ -4,11 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import yoetar.library.viewPicture.KenBurnsView;
+import yoetar.library.viewPicture.Transition;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        KenBurnsView kenBurnsView = findViewById(R.id.IdKenBurnsView);
+        kenBurnsView.setTransitionListener(new KenBurnsView.TransitionListener() {
+            @Override
+            public void onTransitionStart(Transition transition) {
+
+            }
+            @Override
+            public void onTransitionEnd(Transition transition) {
+
+            }
+        });
     }
 }
